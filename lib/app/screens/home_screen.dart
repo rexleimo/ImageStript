1bacb8e3783564f3bd3e291157834b7c309ca888
傻梦兽 <rainbowmorel@163.com>
2026-05-12 00:18:04 +0800

    feat: add Flutter app shell with home screen

---
+++ lib/app/screens/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Stript'),
+        centerTitle: false,
+        actions: [
+          IconButton(icon: const Icon(Icons.settings), onPressed: () {}),
+        ],
+      ),
+      body: const Center(
+        child: Text('Drop images here'),
+      ),
+    );
+  }
+}
